5cbb62eca6ac89075f58dafc7baa956ad328c892
ConradoEC <113607331+ConradoEC@users.noreply.github.com>
2023-11-23 01:16:09 -0300

    Updates on styles

--- forms/index.php
+++ forms/index.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Formulário</title>
-        <link rel="stylesheet" href="style.css">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link rel="stylesheet" href="style.css">
+        <title>Formulário</title>
     </head>
 
     <body>
